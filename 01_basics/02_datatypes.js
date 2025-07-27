ca2563d1ff9cb92bec4a5c1085a517633350abf5
Pallavi patil <130857525+pallavi-pa@users.noreply.github.com>
2025-07-27 09:44:47 +0000

    datatypes updated

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,25 @@
 "use strict"; // treat all JS code as newer version
  
 // alert( 3 + 3) // we are using nodejs, not browser
+console.log(3
+    +
+    3)//code redability should be high 
+console.log("Pallavi")
+let name="Pallavi"
+let age=21
+let isloggedIn = false
+let state; // undefined value, it is not null value
+// number=> 2 to power 53
+// bigint
+//string=> ""
+//boolean => true/false
+//null =>standard value
+//undefined =>
+//undefines
+// symbol=> unque
+// object
+console.log(typeof "Pallavi")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
